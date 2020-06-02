810c41e5d7838490f270d6504af7b64378f8da96
ghenry22 <ghenry22@gmail.com>
2020-06-02 13:03:01 +0800

    add shealth plugin test page

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Platform } from '@ionic/angular';
-
 // import { HealthKit } from '@ionic-native/health-kit/ngx';
 import { Health } from '@ionic-native/health/ngx';
 
@@ -9,12 +8,14 @@ import { Health } from '@ionic-native/health/ngx';
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
+
   platformReady: Boolean = false;
   stepsArray = [];
   stepsAggregated = [];
   stepsFilteredAggregated = [];
   hasData = false;
+
   constructor(private health: Health, private platform: Platform) {
 
   }
@@ -27,73 +28,74 @@ export class Tab1Page {
     const ready = !!await this.platform.ready();
     if (ready) {
       this.health.isAvailable().then(bool => {
-        console.log("available " + bool);
+        console.log('available ' + bool);
         if (bool) {
           this.platformReady = true;
           this.health.requestAuthorization([
-            'distance', 'nutrition',  //read and write permissions
+            'distance', 'nutrition',                  // read and write permissions
             {
-              read: ['steps','height', 'weight'],       //read only permission
-              write: ['height', 'weight']  //write only permission
+              read: ['steps', 'height', 'weight'],    // read only permission
+              write: ['height', 'weight']             // write only permission
             }
           ])
-          .then(res => console.log("response " + res))
-          .catch(e => console.log("error "+e));
+          .then(res => console.log('response ' + res))
+          .catch(e => console.log('error ' + e));
         }
       })
-      .catch(e => console.log("error "+e));
+      .catch(e => console.log('error ' + e));
     }
   }
 
-  getSteps(){
+  getSteps() {
     this.health.query({
       startDate: new Date(new Date().getTime() - 10 * 24 * 60 * 60 * 1000), // ten days ago
       endDate: new Date(), // now
       dataType: 'steps',
       limit: 1000
-    }).then(data=>{
+    }).then(data => {
       console.log(data);
       data.forEach(item => {
-        this.stepsArray.push({date: item.startDate.toISOString(), count: item.value})
+        this.stepsArray.push({date: item.startDate.toISOString(), count: item.value});
       });
-      if(this.stepsArray.length){
-        this.hasData = true
+      if (this.stepsArray.length) {
+        this.hasData = true;
       }
     }).catch(e => {
-      console.log("error "+ e);
-    })
+      console.log('error ' + e);
+      this.checkPlatformReady();
+    });
   }
 
-  getAggregatedSteps(){
+  getAggregatedSteps() {
     this.health.queryAggregated({
       startDate: new Date(new Date().getTime() - 7 * 24 * 60 * 60 * 1000), // ten days ago
       endDate: new Date(), // now
       dataType: 'steps',
       bucket: 'day'
-    }).then(data=>{
+    }).then(data => {
       console.log(data);
       data.forEach(item => {
-          this.stepsAggregated.push({start: item.startDate, end: item.endDate, count: item.value})
+          this.stepsAggregated.push({start: item.startDate, end: item.endDate, count: item.value});
       });
     }).catch(e => {
-      console.log("error "+ e);
-    })
+      console.log('error ' + e);
+    });
   }
 
-  getFilteredAggregatedSteps(){
+  getFilteredAggregatedSteps() {
     this.health.queryAggregated({
       startDate: new Date(new Date().getTime() - 7 * 24 * 60 * 60 * 1000), // ten days ago
       endDate: new Date(), // now
       dataType: 'steps',
       bucket: 'day',
       filtered: true
-    }).then(data=>{
+    }).then(data => {
       console.log(data);
       data.forEach(item => {
-          this.stepsFilteredAggregated.push({start: item.startDate, end: item.endDate, count: item.value})
+          this.stepsFilteredAggregated.push({start: item.startDate, end: item.endDate, count: item.value});
       });
     }).catch(e => {
-      console.log("error "+ e);
-    })
+      console.log('error ' + e);
+    });
   }
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,8 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Platform } from '@ionic/angular';
+declare var shealth: any;
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {}
+export class Tab2Page implements OnInit {
+
+  platformReady: Boolean = false;
+
+  constructor(private platform: Platform) {
+
+  }
+
+  ngOnInit() {
+    this.checkPlatformReady();
+  }
+
+  async checkPlatformReady() {
+    const ready = !!await this.platform.ready();
+    console.log('READY: ' + ready);
+  }
+
+  success(msg) {
+    console.log(msg);
+    alert(msg);
+  }
+
+  error(msg) {
+    alert('ERROR: ' + msg);
+  }
+
+  greet() {
+    shealth.greet('World', this.success, this.error);
+  }
+
+  apiConnect() {
+    shealth.connectToSHealth('', this.success, this.error);
+  }
+
+  apiPermissions() {
+    shealth.callHealthPermissionManager('', this.success, this.error);
+  }
+
+  apiGetData() {
+    const startDate = new Date(new Date().getTime() - 10 * 24 * 60 * 60 * 1000).getTime();
+    const endDate = new Date().getTime();
+    shealth.getDataFromSHealth(startDate, endDate, this.success, this.error);
+  }
+}
